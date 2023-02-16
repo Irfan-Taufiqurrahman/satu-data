f567bee55b75af21fcc1662dc8c3abeaa864a069
Reza1290 <reza.muktasib@gmail.com>
2023-02-16 14:51:54 +0700

    VUE
    GG

---
+++ VUE/Belajar Vue/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Content from './components/SideBar.vue'
+
+
+
+
+</script>
+
+<template>
+    <Content />
+</template>
+<script>
+
+</script>
+<style scoped>
+
+
+</style>
---
+++ VUE/Belajar Vue/src/components/Content.vue
@@ -0,0 +1,27 @@
+<template>
+        <div class="pl-5 pt-5 w-full">
+            <div class="bg-white">
+                <div class="bg-white h-full">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Hello</th>
+                                <th>Hello</th>
+                                <th>Hello</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Hello world</td>
+                                <td>Hello2</td>
+                            </tr>
+                            <tr>
+                                <td>Hello world</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    
+</template>
